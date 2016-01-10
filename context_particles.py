77f35c031c089a17dbaa61030f2cbb8e36d459e1
Michael Chang <mbchang@mit.edu>
2016-01-10 16:18:50 -0500

    added context_particles, which has classes that help recover the data from torch. Previous commits for recovering would not work without this file

---
+++ context_particles.py
@@ -0,0 +1,102 @@
+import numpy as np
+from utils import *
+
+# Hardcoded Global Variables
+G_w_width, G_w_height = 640.0,480.0
+G_max_velocity, G_min_velocity = 2*4500.0, -2*4500.0  # make this double the max initial velocity, there may be some velocities that go over, but those are anomalies
+G_mass_values = [0.33, 1.0, 3.0]  # hardcoded
+G_goo_strength_values = [0.0, -5.0, -20.0]  # hardcoded
+G_goo_strength2color = {0.0: "darkmagenta", -5.0: "brown", -20.0: "yellowgreen"}
+G_particle_mass2color = {0.33: "yellow", 1.0: "red", 3.0: "blue"}
+
+class Context_Goo():
+    def __init__(self, goo_vector):
+        """
+            goo_vector: [cx, cy, width, height, [onehot strength], id]
+
+             Inverse Transformation: normalize --> xywh2ltrb (don't need to crop)
+
+             Reference Forward Transformation
+             goos[:,:4] = crop_to_window(goos[:,:4])  # crop so that dimensions are inside window
+             goos[:,:4] = ltrb2xywh(goos[:,:4])  # convert [left, top, right, bottom] to [cx, cy, w, h]
+             goos[:,:4] = goos[:,:4]/G_w_width  # normalize coordinates
+        """
+        # unpack
+        cx_cy_w_h, one_hot = self.unpack_goo_vector(goo_vector)
+
+        # normalize
+        cx_cy_w_h = cx_cy_w_h*G_w_width
+
+        # xywh2ltrb
+        [self.left, self.top, self.right, self.bottom] = self.xywh2ltrb_one_box(cx_cy_w_h)
+
+        # onehot
+        self.strength = one_hot_to_num(one_hot, G_goo_strength_values) # convert to strength
+        self.color = G_goo_strength2color[self.strength]  # convert to color
+
+    def __str__(self):
+        return 'Goo: left: %s, top: %s, right: %s, bottom: %s, color: %s' \
+                %(self.left, self.top, self.right, self.bottom, self.color)
+
+    def unpack_goo_vector(self, goo_vector):
+        """
+            goo_vector: [cx, cy, width, height, [onehot strength], id]
+        """
+        assert goo_vector.shape == (8,)
+        cx_cy_w_h = goo_vector[:4]
+        one_hot_strength = goo_vector[4:7]
+        object_id = goo_vector[7]
+        assert object_id == 0
+
+        return cx_cy_w_h, one_hot_strength
+
+    def xywh2ltrb_one_box(self, box):
+        """
+            box: [cx, cy, width, height]
+            out: [left, top, right, bottom]
+
+            decoupled from normalization
+        """
+        cx, cy, width, height = box[:]
+        left = cx - width/2
+        right = cx + width/2
+        assert right > left
+        top = cy - height/2
+        bottom = cy + height/2
+        assert bottom > top
+        return [left, top, right, bottom]
+
+    def format(self):
+        return [[self.left, self.top], [self.right, self.bottom],
+                self.strength, self.color]
+
+class Context_Particle():
+    def __init__(self, particle_path):
+        """
+            particle_path: np array (winsize/2, 8)
+        """
+        # Get path
+        self.path = np.copy(particle_path[:, :4])  # (winsize, [px, py, vx, vy])
+        self.path[:,:2] = self.path[:,:2]*G_w_width  # unnormalize position
+        self.path[:,2:] = self.path[:,2:]*G_max_velocity  # unnormalize velocity
+
+        # Get mass
+        one_hot = particle_path[0, 4:7]  # should be the same for all timesteps, so we just use the first one
+        self.mass = one_hot_to_num(one_hot, G_mass_values)
+        assert np.allclose(particle_path[:, 7], 1)  # object id
+        self.color = G_particle_mass2color[self.mass]
+
+    def __str__(self):
+        return "Particle: color: %s" %(self.color)
+
+    def to_dict(self):
+        return {'color': self.color, 'mass': self.mass}
+
+    def reshape_path(self, path):
+        """
+            From: (winsize, [px, py, vx, vy])
+            To: (winsize, [pos vel], [x y])
+        """
+        winsize = path.shape[0]
+        assert path.shape[1] == 4
+        return np.reshape(path, (winsize, 2, 2))
