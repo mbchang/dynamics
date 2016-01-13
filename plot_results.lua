5be15e98841ebfc965c77c13a3fcc05b9cf97a44
Michael Chang <mbchang@mit.edu>
2016-01-13 14:05:29 -0500

    updated plot_results for better plotting

--- plot_results.lua
+++ plot_results.lua
@@ -2,10 +2,11 @@ require 'torch'
 require 'metaparams'
 require 'gnuplot'
 require 'paths'
+require 'utils'
 
 function plot_train_losses(losses_file, plotfolder)
     -- NOTE THAT THIS IS ACROSS ALL EPOCHS
-    if not paths.dirp(plotfolder) then 
+    if not paths.dirp(plotfolder) then
         paths.mkdir(plotfolder)
     end
 
@@ -20,7 +21,10 @@ function plot_train_losses(losses_file, plotfolder)
     end
     skipped_losses = torch.Tensor(skipped_losses)
 
-    gnuplot.pngfigure(plotfolder..'/'..losses_file..'_logskippedlosses.png')
+    -- local plotfile =plotfolder..'/'..losses_file..'_logskippedlosses.png'
+    local plotfile = losses_file..'_logskippedtrainlosses.png'
+
+    gnuplot.pngfigure(plotfile)
     gnuplot.xlabel('batch')
     gnuplot.ylabel('Log MSE Loss')
     gnuplot.title('Losses On Training Set')  -- change
@@ -29,48 +33,81 @@ function plot_train_losses(losses_file, plotfolder)
 end
 
 function plot_experiment_results(experiment_results_file, plotfolder)
-    if not paths.dirp(plotfolder) then 
-        paths.mkdir(plotfolder)
-    end
+    if not paths.dirp(plotfolder) then paths.mkdir(plotfolder) end
 
     local experiment_results = torch.load(experiment_results_file)
     local train_results = {}
     local dev_results = {}
     local all_results = {}
-    for learning_rate, losses in pairs(experiment_results) do 
+    for learning_rate, losses in pairs(experiment_results) do
         train_results[#train_results+1] = {'train '.. learning_rate, torch.log(losses.results_train_losses)}
         dev_results[#dev_results+1] = {'dev '.. learning_rate, torch.log(losses.results_dev_losses)}
         all_results[#all_results+1] = {'train '.. learning_rate, torch.log(losses.results_train_losses)}
         all_results[#all_results+1] = {'dev '.. learning_rate, torch.log(losses.results_dev_losses)}
     end
 
-    gnuplot.pngfigure(plotfolder..'/traintest.png')
-    gnuplot.xlabel('epoch')
-    gnuplot.ylabel('Log MSE Loss')
-    gnuplot.title('Losses On Training Set')
-    gnuplot.plot(unpack(train_results))
-    gnuplot.plotflush()
-
-    gnuplot.pngfigure(plotfolder..'/testest.png')
-    gnuplot.xlabel('epoch')
-    gnuplot.ylabel('Log MSE Loss')
-    gnuplot.title('Losses On Development Set')    
-    gnuplot.plot(unpack(dev_results))
-    gnuplot.plotflush()
+    -- gnuplot.pngfigure(plotfolder..'/traintest.png')
+    -- gnuplot.xlabel('epoch')
+    -- gnuplot.ylabel('Log MSE Loss')
+    -- gnuplot.title('Losses On Training Set')
+    -- gnuplot.plot(unpack(train_results))
+    -- gnuplot.plotflush()
+    --
+    -- gnuplot.pngfigure(plotfolder..'/testest.png')
+    -- gnuplot.xlabel('epoch')
+    -- gnuplot.ylabel('Log MSE Loss')
+    -- gnuplot.title('Losses On Development Set')
+    -- gnuplot.plot(unpack(dev_results))
+    -- gnuplot.plotflush()
 
     gnuplot.pngfigure(plotfolder..'/alltest.png')
     gnuplot.xlabel('epoch')
     gnuplot.ylabel('Log MSE Loss')
-    gnuplot.title('Losses')    
+    gnuplot.title('Losses')
     gnuplot.plot(unpack(all_results))
     gnuplot.plotflush()
 end
+
+function plot_all_experiments(parent_folder, plotfn, logfile)
+    for experiment_folder in paths.iterdirs(parent_folder) do
+        local plot_folder = paths.concat(parent_folder, experiment_folder)
+        -- local er_file = plot_folder..'/experiment_results.t7'
+        -- plot_experiment_results(er_file, plot_folder)
+        local er_file = plot_folder..'/'..logfile
+        plotfn(er_file, plot_folder)
+    end
+end
+
+
+function plot_all_training(parent_folder)
+    local results_files = {'losses,lr=0.0005_results',
+                            'losses,lr=5e-05_results',
+                            'losses,lr=5e-06_results'}
+    for _, train_results in pairs(results_files) do
+
+        try {
+            plot_all_experiments(parent_folder, plot_train_losses, train_results),
+        catch {
+              function(error)
+                 print('caught error: ' .. error)
+              end
+           }
+        }
+    end
+end
+
+
+
+-- losses,lr=0.0005_results.t7  losses,lr=5e-06_results.t7   saved_model,lr=5e-05.t7
+-- experiment_results.t7        losses,lr=5e-05_results.t7
 
 -- assert(false)
 -- common_mp.results_folder = 'rand_order_results_batch_size=100_seq_length=10_layers=4_rnn_dim=100'
 -- print(common_mp.results_folder)
 
 -- plot_experiment_results(common_mp.results_folder .. '/experiment_results.t7', 'plots')
-plot_train_losses(common_mp.results_folder .. '/losses,lr=5e-05_results', '.')
+-- plot_train_losses(common_mp.results_folder .. '/losses,lr=5e-05_results', '.')
 
 
+plot_all_training('openmind')
+-- plot_all_experiments('openmind', plot_experiment_results, 'experiment_results.t7')
