d7e15e36d2dd422885dcd24ea8f4338a1204f02a
Michael Chang <mbchang@mit.edu>
2016-01-14 09:01:41 -0500

    removed try catch with manual checks

--- predict.lua
+++ predict.lua
@@ -1,9 +1,58 @@
+require 'utils'
+
 local Tester = require 'test'
+--
+-- local t =  Tester.create('testset', test_mp)  -- will choose an example based on random shuffle or not
+-- -- local model = 'results_batch_size=100_seq_length=10_layers=2_rnn_dim=100_max_epochs=20/saved_model,lr=0.0005.t7'
+-- -- local model = 'results_batch_size=1_seq_length=10_layers=4_rnn_dim=100_max_epochs=10debug_curriculum/saved_model,lr=0.00025.t7'
+--
+-- -- local model = 'pc/results_batch_size=1_seq_length=10_layers=4_rnn_dim=100_max_epochs=50trainloss_gt_devloss/saved_model,lr=0.0005.t7'
+--
+-- local model = 'logs/results_batch_size=100_seq_length=10_layers=2_rnn_dim=100_max_epochs=20floatnetwork/saved_model,lr=0.0005.t7'
+--
+-- t:test(model, 1, true)
 
-local t =  Tester.create('testset', test_mp)  -- will choose an example based on random shuffle or not
--- local model = 'results_batch_size=100_seq_length=10_layers=2_rnn_dim=100_max_epochs=20/saved_model,lr=0.0005.t7'
--- local model = 'results_batch_size=1_seq_length=10_layers=4_rnn_dim=100_max_epochs=10debug_curriculum/saved_model,lr=0.00025.t7'
+function predict(parent_folder, experiment_folder, logfile)
+    local t = Tester.create('testset', test_mp)
 
-local model = 'pc/results_batch_size=1_seq_length=10_layers=4_rnn_dim=100_max_epochs=50trainloss_gt_devloss/saved_model,lr=0.0005.t7'
+    -- hacky
+    local start = experiment_folder:find('layers=') + #'layers='
+    local finish = experiment_folder:find('_rnn_dim=') - 1
+    local num_layers = tonumber(experiment_folder:sub(start, finish))
+    if num_layers == 4 then
+        common_mp.layers = 4
+    else
+        common_mp.layers = 2
+    end
 
-t:test(model, 1, true)
+    local model = parent_folder..'/'..experiment_folder..'/'..logfile
+    t:test(model,1,true)
+    if common_mp.cuda then cutorch.synchronize() end
+    collectgarbage()
+end
+
+
+function predict_all_experiments(parent_folder, logfile)
+    for experiment_folder in paths.iterdirs(parent_folder) do
+        if experiment_folder:sub(1,#'results') == 'results' then
+            local files = paths.dir(parent_folder..'/'..experiment_folder)
+            for _, file in pairs(files) do
+                if file == logfile then
+                    predict(parent_folder, experiment_folder, logfile)
+                end
+            end
+        end
+    end
+end
+
+
+function predict_all_models(parent_folder)
+    local saved_models = {'saved_model,lr=0.0005.t7',
+                            'saved_model,lr=5e-05.t7',
+                            'saved_model,lr=5e-06.t7'}
+    for _, saved_model in pairs(saved_models) do
+        predict_all_experiments(parent_folder, saved_model)
+    end
+end
+
+predict_all_models('logs')
