adf47802b72412f092416fc921bf9eab9a6e3e63
Michael Chang <mbchang@mit.edu>
2016-01-15 22:10:29 -0500

    added logging utils

---
+++ logging_utils.lua
@@ -0,0 +1,24 @@
+local T = require 'pl.tablex'
+
+function merge_tables(t1, t2)
+    -- Merges t2 and t1, overwriting t1 keys by t2 keys when applicable
+    local merged_table = T.deepcopy(t1)
+    for k,v in pairs(t2) do
+        -- if merged_table[k] then
+        --     error('t1 and t2 both contain the key: ' .. k)
+        -- end
+        merged_table[k]  = v
+    end
+    return merged_table
+end
+
+function create_experiment_string(keys, params)
+    print(keys)
+    print(params)
+    local foldername = 'results'
+    for i=1,#keys do
+        print(params[keys[i]])
+        foldername = foldername .. '_'..keys[i]..'='..params[keys[i]]
+    end
+    return foldername
+end
