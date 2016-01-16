a68b093a904266a34e920936e1f14d223077e2ea
Michael Chang <mbchang@mit.edu>
2016-01-16 18:26:15 -0500

    cleaned up

--- experiment_new.lua
+++ experiment_new.lua
@@ -16,9 +16,8 @@ require 'pl'
 -- missing params: seed, experiment_string
 -- also booleans are strings somehow
 -- you can
---------------------------- Init ------------------------------
+------------------------------------- Init -------------------------------------
 require 'logging_utils'
-
 
 mp = lapp[[
    -d,--savedir       (default "logs")      	subdirectory to save logs
@@ -37,12 +36,6 @@ mp = lapp[[
    --save_every		  (default 50)
    --print_every	  (default 10)
 ]]
-
-
--- torch.manualSeed(123)
--- if params.rand_init_wts then torch.manualSeed(123) end
-if mp.shuffle == 'false' then mp.shuffle = false end
-if mp.rand_init_wts == 'false' then mp.rand_init_wts = false end
 
 if mp.server == 'pc' then
 	mp.winsize = 10
@@ -66,6 +59,12 @@ mp.input_dim = 8.0*mp.winsize/2
 mp.out_dim = 8.0*mp.winsize/2
 mp.results_folder = create_experiment_string({'batch_size', 'seq_length', 'layers', 'rnn_dim', 'max_epochs'}, mp) .. 'new_test'
 
+if mp.rand_init_wts then torch.manualSeed(123) end
+if mp.shuffle == 'false' then mp.shuffle = false end
+if mp.rand_init_wts == 'false' then mp.rand_init_wts = false end
+if mp.cuda then require 'cutorch' end
+if mp.cunn then require 'cunn' end
+
 local optim_state = {learningRate   = mp.lr,
                      momentumDecay  = 0.1,
                      updateDecay    = 0.01}
@@ -74,44 +73,19 @@ local D = require 'DataLoader'
 local M = require 'model_new'
 local model, train_loader, test_loader
 
--- threads
--- torch.setnumthreads(common_mp.num_threads)
--- print('<torch> set nb of threads to ' .. torch.getnumthreads())
-
--- local Tester = require 'test'
--- local Trainer = require 'train'
-
-
-common_mp = tablex.deepcopy(mp)
-train_mp = tablex.deepcopy(mp)  -- problem: something in train_mp got mutated in Trainer
-test_mp = tablex.deepcopy(mp)
-
-if common_mp.cuda then require 'cutorch' end
-if common_mp.cunn then require 'cunn' end
-
-
--- local trainer = Trainer.create('trainset', train_mp)  -- need to specify learning rate here
--- local trainer_tester = Tester.create('trainset', test_mp)
--- local tester = Tester.create('testset', test_mp)
-
--- local learning_rates = {5e-4, 5e-5, 5e-6}
-
-local experiment_results = common_mp.results_folder .. '/experiment_results.t7'
-
--- if not common_mp.rand_init_wts then torch.manualSeed(123) end
-torch.manualSeed(123)
+------------------------------- Helper Functions -------------------------------
 
 -- initialize
 function init(preload, model_path)
-  print("Network parameters:")
-  print(mp)
-  train_loader = D.create('trainset', mp.dataset_folder, {}, mp.batch_size, mp.curriculum, mp.shuffle)  -- TODO: take care of all curriculum here
-  test_loader = D.create('testset', mp.dataset_folder, {}, mp.batch_size, mp.curriculum, mp.shuffle)  -- TODO: take care of all curriculum here
-  model = M.create(mp, preload, model_path)
-  local epoch = 0  -- TODO Not sure if this is necessary
-  local beginning_time = torch.tic() -- TODO not sure if this is necessary
-  local start_time = torch.tic()  -- TODO not sure if this is necessary
-  print("Initialized Network")
+    print("Network parameters:")
+    print(mp)
+    train_loader = D.create('trainset', mp.dataset_folder, {}, mp.batch_size, mp.curriculum, mp.shuffle)  -- TODO: take care of all curriculum here
+    test_loader = D.create('testset', mp.dataset_folder, {}, mp.batch_size, mp.curriculum, mp.shuffle)  -- TODO: take care of all curriculum here
+    model = M.create(mp, preload, model_path)
+    local epoch = 0  -- TODO Not sure if this is necessary
+    local beginning_time = torch.tic() -- TODO not sure if this is necessary
+    local start_time = torch.tic()  -- TODO not sure if this is necessary
+    print("Initialized Network")
 end
 
 -- closure
@@ -124,43 +98,17 @@ function feval_train(params_)  -- params_ should be first argument
     return loss, grad -- f(x), df/dx
 end
 
--- closure
--- returns loss, grad_params
-function feval_test(params_)  -- params_ should be first argument
-    local this, context, y, mask = unpack(test_loader:next_batch()) -- TODO this should take care of curriculum, how to deal with dataloader?
-    local loss, state, predictions = model:fp(params_, {this=this,context=context}, y)
-    local grad = model:bp({this=this,context=context}, y, mask, state)
-    collectgarbage()
-    return loss, grad -- f(x), df/dx
-end
-
 --TODO what? optim.rmsprop is different from rmsprop?
 
 function train(epoch_num)
     local cntr = 0, new_params, train_loss
-    for t = 1,train_loader.num_batches do  -- TODO
-        -- if t == 2 then
-        --     print('params', model.theta.params)
-        --     assert(false)
-        -- end
-        -- local optim_state = {learningRate   = mp.lr,
-        --                      momentumDecay  = 0.1,
-        --                      updateDecay    = 0.01}
-        -- print('optim_state before', optim_state)
-        -- assert(false)
-
+    for t = 1,train_loader.num_batches do
         --   xlua.progress(t, dataloader.num_batches)
         new_params, train_loss = rmsprop(feval_train, model.theta.params, optim_state)  -- next batch
-        assert(new_params == model.theta.params)  -- TODO remove later
-        --   if params.plot and math.fmod(cntr, 20) == 0 then test() end
-
-        -- print('params', model.theta.params)
-        -- assert(false)
-
+        assert(new_params == model.theta.params)
         if t % mp.print_every == 0 then
             print(string.format("epoch %2d\titeration %2d\tloss = %6.8f\tgradnorm = %6.4e", epoch_num, t, train_loss[1], model.theta.grad_params:norm()))
         end
-
 
         if t % mp.save_every == 0 then
             -- convert from cuda to float before saving
@@ -177,32 +125,28 @@ function train(epoch_num)
             -- torch.save(self.logs.lossesfile, self.logs.train_losses)
         end
 
-
         cntr = cntr + 1
         -- trainLogger:plot()
-        if common_mp.cuda then cutorch.synchronize() end
+        if mp.cuda then cutorch.synchronize() end
         collectgarbage()
     end
     return train_loss[1]  -- because train_loss is returned as a table
 end
 
 
-function test()
-    -- print(model.theta.params)
-    -- assert(false)
+function test(dataloader)
     local sum_loss = 0
-    for i = 1,test_loader.num_batches do
+    for i = 1,dataloader.num_batches do
         -- xlua.progress(t, test_loader.num_batches)
-        local this, context, y, mask = unpack(test_loader:next_batch()) -- TODO this should take care of curriculum, how to deal with dataloader?
+        local this, context, y, mask = unpack(dataloader:next_batch()) -- TODO this should take care of curriculum, how to deal with dataloader?
         local test_loss, state, predictions = model:fp(model.theta.params, {this=this,context=context}, y)
         sum_loss = sum_loss + test_loss
 
         -- here you have the option to save predictions into a file
         local prediction = predictions[torch.find(mask,1)[1]] -- (1, windowsize/2)
-        -- this would be your 'this', or you could shift over, or do other interesting things
 
         -- reshape to -- (num_samples x windowsize/2 x 8)
-        prediction = prediction:reshape(this:size(1), mp.winsize/2, test_loader.object_dim)
+        prediction = prediction:reshape(this:size(1), mp.winsize/2, dataloader.object_dim)
 
         -- if saveoutput then
         --     assert(torch.type(model)=='string')
@@ -211,22 +155,26 @@ function test()
         --                         {'model_predictions', model})
         -- end
     end
-    local avg_loss = sum_loss/test_loader.num_batches
+    local avg_loss = sum_loss/dataloader.num_batches
     collectgarbage()
     return avg_loss
 end
 
-init(false)
+------------------------------------- Main -------------------------------------
 
+torch.setnumthreads(mp.num_threads)
+print('<torch> set nb of threads to ' .. torch.getnumthreads())
+
+local experiment_results = mp.results_folder .. '/experiment_results.t7'
 local all_results = {}
-
--- for index, learning_rate in pairs(learning_rates) do
 local learning_rate = mp.lr
 print('Learning rate:', learning_rate)
 local train_losses = {}
 local dev_losses = {}
-
 local oldp, oldgp
+
+init(false)
+
 
 for i = 1, mp.max_epochs do
     if i == 3 then assert(false) end
@@ -235,20 +183,17 @@ for i = 1, mp.max_epochs do
     -- this train_loss is the final loss after one epoch. We expect to see this go down as epochs increase
     -- local model
     local train_loss
-    -- print(optim_state)
     train_loss = train(i)  -- trainer.train_loader.num_batches
     -- Get the training loss
     -- local train_loss = trainer_tester:test(model, p, trainer_tester.test_loader.num_batches)  -- tester.test_loader.nbatches  -- creating new copy of model when I load into Tester!
     -- local train_loss = trainer_tester:test(model, trainer_tester.test_loader.num_batches)  -- tester.test_loader.nbatches  -- creating new copy of model when I load into Tester!
     print('train loss\t', train_loss)
-    -- print(model.theta.params)
-    -- assert(false)
 
     -- Test
     -- this train_loss is the final loss after one epoch. We expect to see this go in a parabola as epochs increase
-    -- local dev_loss = tester:test(model, p, tester.test_loader.num_batches)  -- tester.test_loader.nbatches  -- creating new copy of model when I load into Tester!
-    -- local dev_loss = tester:test(model, tester.test_loader.num_batches)  -- tester.test_loader.nbatches  -- creating new copy of model when I load into Tester!
-    local dev_loss = test()
+    local dev_loss = test(test_loader)
+
+    -- here reset the test_loader (make current_batch = 0)
 
     -- Record loss
     train_losses[#train_losses+1] = train_loss
@@ -265,7 +210,6 @@ for i = 1, mp.max_epochs do
     print('all_results', all_results)
     -- torch.save(experiment_results, all_results)
 
-    if common_mp.cuda then cutorch.synchronize() end
+    if mp.cuda then cutorch.synchronize() end
     collectgarbage()
 end
--- end
