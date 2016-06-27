f2f72538892e61cbdde8990128c3809fdf924cee
Michael Chang <mbchang@mit.edu>
2016-06-26 17:04:46 -0700

    config for loading matter-js

---
+++ config.lua
@@ -0,0 +1,11 @@
+local args = {position_normalize_constant=800,
+        velocity_normalize_cnstant=50,
+        relative=true,
+        masses={0.33, 1.0, 3.0, 1e30},
+        rsi={px=1, py=2, vx=3, vy=4, m=5, oid=6},  -- raw state indicies
+        si={px=1, py=2, vx=3, vy=4, m={5,8}, oid=9},  -- state indices
+        permute_context=False,
+        batch_size=4,
+        shuffle=False}
+
+return args
