067dac88d8ab8810eba632809f4fb4799a0e6404
Michael Chang <mbchang@mit.edu>
2016-08-04 12:19:23 -0700

    demo config in Demo_minimal

--- demo/js/Demo_minimal.js
+++ demo/js/Demo_minimal.js
@@ -66,14 +66,21 @@
         // Render.run(demo.render)
 
         demo.offset = 5;  // world offset
-        // demo.cx = 400;
-        // demo.cy = 300;
+        demo.config = {}
+        demo.config.cx = 400;
+        demo.config.cy = 300;
+        demo.config.masses = [1, 5, 25]
+        demo.config.mass_colors = {'1':'#C7F464', '5':'#FF6B6B', '25':'#4ECDC4'}
+        demo.config.sizes = [0.5, 1, 2]  // multiples
+        demo.config.object_base_size = {'ball': 60, 'obstacle': 120, 'block': 40 }  // radius of ball, side of square obstacle, long side of block
+        demo.config.objtypes = ['ball', 'obstacle', 'block']  // squares are obstacles
+        demo.config.g = 0 // default? [0,1] Or should we make this a list? The index of the one hot. 0 is no, 1 is yes
+        demo.config.f = 0 // default? [0,1]
+        demo.config.p = 0 // default? [0,1,2]
+        demo.config.max_velocity = 60
 
-        config.cx = 400;
-        config.cy = 300;
-
-        demo.cx = config.cx;
-        demo.cy = config.cy;
+        demo.cx = demo.config.cx;
+        demo.cy = demo.config.cy;
         demo.width = 2*demo.cx
         demo.height = 2*demo.cy
 
