034e6b4fc79ce2edb12303f5f1bff59543097db5
Michael Chang <mbchang@mit.edu>
2016-08-26 11:11:54 -0700

    can create video for Demo_minimal output. Next is to formalize paths

--- create_video.py
+++ create_video.py
@@ -34,9 +34,27 @@ def create_gif(images_root):
     file_names = sorted([fn for fn in os.listdir(images_root) if fn.endswith('.png')], key=lambda x: img_id(x))
     images = [Image.open(os.path.join(images_root,fn)) for fn in file_names]
     filename = os.path.join(images_root, "gif.GIF")
-    # print filename
     writeGif(filename, images, duration=0.2)
 
+
+def img_id_json(filename):
+    # print(filename)
+    begin = filename.rfind('_')+1
+    end = filename.rfind('.')
+    return int(filename[begin:end])
+
+def create_gif_json(images_root):
+    """
+        writeGif(filename, images, duration=0.1, loops=0, dither=1)
+            Write an animated gif from the specified images.
+            images should be a list of numpy arrays of PIL images.
+            Numpy images of type float should have pixels between 0 and 1.
+            Numpy images of other types are expected to have values between 0 and 255.
+    """
+    file_names = sorted([fn for fn in os.listdir(images_root) if fn.endswith('.png')], key=lambda x: img_id_json(x))
+    images = [Image.open(os.path.join(images_root,fn)) for fn in file_names]
+    filename = os.path.join(images_root, "gif.GIF")
+    writeGif(filename, images, duration=0.2)
 
 def create_grid(images_root):
     base_dim = 100
@@ -133,4 +151,14 @@ if __name__ == '__main__':
     # create_grid_mutation(root + 'batch_60_input_1_along_16')
 
 
-    create_gif('/Users/MichaelChang/Dropbox (MIT Solar Car Team)/MacHD/Documents/Important/MIT/Research/SuperUROP/Code/dynamics/oplogs/18_layers_3_lrdecay_0.99_dataset_folder_14_34balls_lr_0.0003_sharpen_1_model_lstmobj/videos')
+    # create_gif('/Users/MichaelChang/Dropbox (MIT Solar Car Team)/MacHD/Documents/Important/MIT/Research/SuperUROP/Code/dynamics/oplogs/18_layers_3_lrdecay_0.99_dataset_folder_14_34balls_lr_0.0003_sharpen_1_model_lstmobj/videos')
+
+    create_gif_json('/Users/MichaelChang/Documents/Researchlink/SuperUROP/Code/physics_worlds/demo/js')
+
+
+
+
+
+
+
+
