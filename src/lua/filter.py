feac1914a8af33a135635be44950e629d85dfbb3
Michael Chang <mbchang@mit.edu>
2016-11-23 10:55:19 -0500

    remove python files

--- src/lua/filter.py
+++
@@ -1,3 +0,0 @@
-for line in sorted(open('blstmtower.txt','r').readlines()):
-    if 'mixed_n6_t60_ex50000_m_z_o_dras3_rda__mixed_n6_t60_ex50000_m_z_o_dras3' in line and 'modelbl' not in line:
-        print line.strip()
--- src/lua/sort.py
+++
@@ -1,3 +0,0 @@
-for line in sorted(open('scratch2.txt','r').readlines()):
-    # if 'mixed_n3_t60_ex50000_m_z_o_dras3_rda,mixed_n4_t60_ex50000_m_z_o_dras3_rda__mixed_n5_t60_ex50000_m_z_o_dras3_rda' in line:
-        print line.strip()
